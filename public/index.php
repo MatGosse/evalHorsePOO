c61e9a2b97eb48296792bcb90c74e106ae612273
mgosse <mathieu.gosse@forlabs.fr>
2022-11-03 15:45:06 +0100

    Patch Address namespace & Feature add Humans class

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Model\Raider;
 use App\Model\Sheitland;
+use App\Model\Address;
 
 require_once("../src/app.php");
 
 
 
-echo $findus->getId() . "\n";
-echo $findus2->getId();
+$bobAddress = new Address("1 rue random","randomville",14000);
+$bob = new Raider("bob", $bobAddress);
 
+// $findus = new Sheitland("findus", 'White', 200);
+
+
+// echo $findus->getId() . "\n";
+// echo $findus2->getId();
