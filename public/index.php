f884651f5328b25b9c498537e7dcae0ed0c5b3c7
mgosse <mathieu.gosse@forlabs.fr>
2022-11-03 15:35:31 +0100

    Feature implement Address

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ use App\Model\Sheitland;
 
 require_once("../src/app.php");
 
-$findus = new Sheitland("findus", 'White', 200);
-$findus2 = new Sheitland("findus", 'qds', 200);
 
 
 echo $findus->getId() . "\n";
